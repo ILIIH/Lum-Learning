5bb7b26a3ffc1e31ff5e16043b4e9b1799e42b0e
ILLIA <69596138+ILIIH@users.noreply.github.com>
2024-02-13 23:05:55 +0000

    LL-11.2 Create theme importance Fragment (#62)

--- add_theme_ui/src/main/java/com/example/add_theme_ui/fragments/ThemeImportanceFragment.kt
+++ add_theme_ui/src/main/java/com/example/add_theme_ui/fragments/ThemeImportanceFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.add_theme_ui.R
 import com.example.add_theme_ui.databinding.FragmentThemeImportanceBinding
+import com.example.add_theme_ui.viewModels.ThemeAddViewModel
+import org.koin.android.ext.android.inject
 
 class ThemeImportanceFragment : Fragment() {
 
-
+    private val viewModel: ThemeAddViewModel by inject()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = FragmentThemeImportanceBinding.inflate(inflater,container, false)
+        view.highImportanceCard.setOnClickListener {
+            viewModel.setThemeImportance(getString(R.string.high))
+            findNavController().navigate(R.id.to_themeTypeFragment)
+        }
+        view.middleLevelCard.setOnClickListener {
+            viewModel.setThemeImportance(getString(R.string.middle))
+            findNavController().navigate(R.id.to_themeTypeFragment)
+        }
+        view.lowImportanceCard.setOnClickListener {
+            viewModel.setThemeImportance(getString(R.string.low))
+            findNavController().navigate(R.id.to_themeTypeFragment)
+        }
         return view.root
     }
 
