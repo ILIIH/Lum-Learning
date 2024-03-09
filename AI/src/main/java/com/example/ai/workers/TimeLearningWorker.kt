8726f6e162cc5b3f5362a60823a72883d83f00bc
illiabranchuk <jointlord0@gmail.com>
2024-03-09 18:30:36 +0000

    LL-19 Gradle refactoring

--- AI/src/main/java/com/example/ai/workers/TimeLearningWorker.kt
+++ AI/src/main/java/com/example/ai/workers/TimeLearningWorker.kt
@@ -1,7 +1,6 @@
 package com.example.ai.workers
 
 import android.content.Context
-import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.ai.base_ai_classes.*
